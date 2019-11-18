3a60c75a1a382701a70e9c6a6012edbb3b38113b
pedrojms <pmendoza@espol.edu.ec>
2019-11-18 18:15:19 -0500

    Board and spaceship movements

---
+++ VirtualGame/Assets/Scripts/Spaceship.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spaceship : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("left"))
+        { // Move left
+            gameObject.transform.Translate(-4f * Time.deltaTime, 0, 0);
+            
+        }
+
+        if (Input.GetKey("right"))
+        { // Move Right
+            gameObject.transform.Translate(4f * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("up"))
+        { // Move Up
+            gameObject.transform.Translate(0,4f * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey("down"))
+        { // Move Up
+            gameObject.transform.Translate(0, -4f * Time.deltaTime, 0);
+        }
+
+    }
+}
